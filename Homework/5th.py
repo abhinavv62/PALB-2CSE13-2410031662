f4bee888555b492ac3db327f92f407ab7499d74b
abhinavv62 <abhinavprajapati0718@gmail.com>
2026-02-11 14:32:27 +0530

    Create 5th.py

---
+++ Homework/5th.py
@@ -0,0 +1,14 @@
+def rotate(arr):
+    if len(arr) == 0:
+        return arr
+    
+    last = arr[-1]
+    for i in range(len(arr)-1, 0, -1):
+        arr[i] = arr[i-1]
+    arr[0] = last
+    return arr
+
+
+# Example
+arr = [1, 2, 3, 4, 5]
+print(rotate(arr))   # Output: [5, 1, 2, 3, 4]
