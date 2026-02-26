be7d90e804cade5d080e5f269675188a88650771
abhinavv62 <abhinavprajapati0718@gmail.com>
2026-02-26 14:30:06 +0530

    Create PROBLEM35.py

---
+++ ASSIGNMENT-HOMEWORK/ASSIGNMENT7/PROBLEM35.py
@@ -0,0 +1,10 @@
+from collections import defaultdict
+
+def group_anagrams(strs):
+    anagrams = defaultdict(list)
+    
+    for word in strs:
+        key = "".join(sorted(word))
+        anagrams[key].append(word)
+        
+    return list(anagrams.values())
